39fbf2add22a5f318ec2c7639d7791f9f5e1e3dd
Sagar Mainkar <sagar.mainkar@gmail.com>
2020-10-09 15:07:59 +0530

    Working version with all claim buttons in Ticket

--- src/components/Row.js
+++ src/components/Row.js
@@ -1,85 +1,33 @@
 import React, { useState } from "react";
 import Square from "./Square";
 import { makeStyles } from "@material-ui/core/styles";
-import Fab from "@material-ui/core/Fab";
-import { firebase } from "../config/firebase";
-import { useDocumentData } from "react-firebase-hooks/firestore";
 
 const useStyles = makeStyles((theme) => ({
 	margin: {
-		margin: theme.spacing(0.5),
-		marginTop: 0,
+		margin: theme.spacing(1),
+		padding: 0,
+		paddingTop: theme.spacing(0.2),
+		paddingLeft: theme.spacing(0.5),
 	},
 }));
 
-const Row = ({ key, numbers }) => {
+const Row = ({ key, numbers, onRowChange }) => {
 	const [complete, setComplete] = useState(false);
-	const [markedMap, setMarkedMap] = useState(new Map());
 	const classes = useStyles();
 
-	const gameId = "gjGLugGl8ZoQP9EdooNb";
-	const [game, loading, error] = useDocumentData(
-		firebase.firestore().doc(`games/${gameId}`),
-		{
-			snapshotListenOptions: { includeMetadataChanges: true },
-		}
-	);
-
-	const drawnNumbers = game ? game.drawnNumbers : [];
-
 	let squares = [];
-	// let markedMap = [];
-	const checkAllMarked = () => {
-		// console.log(markedMap);
-		let allMarked = true;
-		markedMap.forEach(function (marked, num) {
-			// console.log(`Num: ${num} Marked :${marked}`);
-			if (!marked) allMarked = false;
-		});
 
-		return allMarked;
-	};
 	const onChange = (num, marked) => {
-		// console.log(`Num:${num} Marked:${marked}`);
-		setMarkedMap(markedMap.set(num, marked));
-		// console.log(checkAllMarked());
-		setComplete(checkAllMarked());
+		onRowChange(num, marked);
 	};
 
 	numbers.forEach((num, index) => {
 		squares.push(<Square key={index} value={num} onChange={onChange} />);
-		if (markedMap.get(num) === undefined && num > 0) {
-			markedMap.set(num, false);
-		}
 	});
-
-	const claimRow = () => {
-		let allIncluded = true;
-		console.log(drawnNumbers);
-		drawnNumbers.forEach((num) =>
-			markedMap.has(num) ? "" : (allIncluded = false)
-		);
-
-		if (allIncluded) {
-		} else {
-			console.log("Not all numbers are drawn yet");
-		}
-	};
 
 	return (
 		<div className="board-row" key={key}>
 			{squares}
-			<Fab
-				variant="extended"
-				size="small"
-				color="primary"
-				aria-label="add"
-				className={classes.margin}
-				disabled={!complete}
-				onClick={(e) => claimRow()}
-			>
-				Claim
-			</Fab>
 		</div>
 	);
 };
--- src/components/Ticket.js
+++ src/components/Ticket.js
@@ -1,8 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import ClaimPrize from "./ClaimPrize";
 import Row from "./Row";
-import Fab from "@material-ui/core/Fab";
 import { makeStyles } from "@material-ui/core/styles";
+import ButtonGroup from "@material-ui/core/ButtonGroup";
+import Button from "@material-ui/core/Button";
+
+import { firebase } from "../config/firebase";
+import { useDocumentData } from "react-firebase-hooks/firestore";
 
 const useStyles = makeStyles((theme) => ({
 	margin: {
@@ -12,9 +16,36 @@ const useStyles = makeStyles((theme) => ({
 
 		display: "block",
 	},
+	root: {
+		display: "flex",
+		flexDirection: "column",
+		alignItems: "center",
+		"& > *": {
+			margin: theme.spacing(1),
+		},
+	},
 }));
 
 const Ticket = ({ numbers, ticketId }) => {
+	const gameId = "gjGLugGl8ZoQP9EdooNb";
+	const [game, loading, error] = useDocumentData(
+		firebase.firestore().doc(`games/${gameId}`),
+		{
+			snapshotListenOptions: { includeMetadataChanges: true },
+		}
+	);
+
+	const drawnNumbers = game ? game.drawnNumbers : [];
+	const [fullHouse, setFullHouse] = useState(false);
+	const [FR, setFR] = useState(false);
+	const [SR, setSR] = useState(false);
+	const [TR, setTR] = useState(false);
+
+	const [markedMap, setMarkedMap] = useState(new Map());
+	const [firstRowMap, setFirstRowMap] = useState(new Map());
+	const [secondRowMap, setSecondRowMap] = useState(new Map());
+	const [thirdRowMap, setThirdRowMap] = useState(new Map());
+
 	const classes = useStyles();
 
 	const firstRow = numbers.slice(0, 7);
@@ -24,25 +55,163 @@ const Ticket = ({ numbers, ticketId }) => {
 	secondRow.unshift(-1);
 	secondRow.push(-1);
 
+	numbers.forEach((num) => {
+		if (markedMap.get(num) === undefined && num > 0) {
+			markedMap.set(num, false);
+		}
+	});
+
+	firstRow.forEach((num) => {
+		if (firstRowMap.get(num) === undefined && num > 0) {
+			firstRowMap.set(num, false);
+		}
+	});
+
+	secondRow.forEach((num) => {
+		if (secondRowMap.get(num) === undefined && num > 0) {
+			secondRowMap.set(num, false);
+		}
+	});
+
+	thirdRow.forEach((num) => {
+		if (thirdRowMap.get(num) === undefined && num > 0) {
+			thirdRowMap.set(num, false);
+		}
+	});
+
+	const onChange = (num, marked) => {
+		setMarkedMap(markedMap.set(num, marked));
+
+		if (firstRowMap.has(num)) {
+			setFirstRowMap(firstRowMap.set(num, marked));
+		}
+		if (secondRowMap.has(num)) {
+			setSecondRowMap(secondRowMap.set(num, marked));
+		}
+		if (thirdRowMap.has(num)) {
+			setThirdRowMap(thirdRowMap.set(num, marked));
+		}
+		// console.log(checkAllMarked());
+		setFullHouse(isFullHouse());
+
+		let [fr, sr, tr] = isRowComplete();
+		// console.log(fr, sr, tr);
+
+		setFR(fr);
+		setSR(sr);
+		setTR(tr);
+	};
+
+	const isFullHouse = () => {
+		let fullHouse = true;
+		markedMap.forEach(function (marked, num) {
+			// if (!marked) console.log(`Num: ${num} Marked :${marked}`);
+			if (!marked) fullHouse = false;
+		});
+
+		return fullHouse;
+	};
+
+	const isRowComplete = () => {
+		let frComplete = true;
+		let srComplete = true;
+		let trComplete = true;
+
+		firstRowMap.forEach(function (marked, num) {
+			if (!marked) frComplete = false;
+		});
+
+		secondRowMap.forEach(function (marked, num) {
+			if (!marked) srComplete = false;
+		});
+
+		thirdRowMap.forEach(function (marked, num) {
+			if (!marked) trComplete = false;
+		});
+
+		return [frComplete, srComplete, trComplete];
+	};
+
+	const claimPrize = (type) => {
+		let allIncluded = true;
+
+		switch (type) {
+			case 1:
+				drawnNumbers.forEach((num) =>
+					firstRowMap.has(num) ? "" : (allIncluded = false)
+				);
+				break;
+			case 2:
+				drawnNumbers.forEach((num) =>
+					secondRowMap.has(num) ? "" : (allIncluded = false)
+				);
+				break;
+			case 3:
+				drawnNumbers.forEach((num) =>
+					thirdRowMap.has(num) ? "" : (allIncluded = false)
+				);
+				break;
+			case 4:
+				drawnNumbers.forEach((num) =>
+					markedMap.has(num) ? "" : (allIncluded = false)
+				);
+				break;
+			default:
+				allIncluded = false;
+				break;
+		}
+
+		if (allIncluded) {
+		} else {
+			console.log("Not all numbers are drawn yet");
+		}
+	};
+
 	return (
 		<>
 			<div className="ticket">
 				<h2>Ticket Number: {ticketId}</h2>
-				<Row numbers={firstRow} key="row1" />
+				<Row numbers={firstRow} key="row1" onRowChange={onChange} />
 
-				<Row numbers={secondRow} key="row2" />
-				<Row numbers={thirdRow} key="row3" />
+				<Row numbers={secondRow} key="row2" onRowChange={onChange} />
+				<Row numbers={thirdRow} key="row3" onRowChange={onChange} />
 			</div>
-
-			<Fab
-				variant="extended"
-				size="small"
-				color="primary"
-				aria-label="add"
-				className={classes.margin}
-			>
-				Claim Full house
-			</Fab>
+			<div className={classes.root}>
+				<ButtonGroup size="small" aria-label="small outlined button group">
+					<Button
+						variant="contained"
+						color="primary"
+						disabled={!FR}
+						onClick={(e) => claimPrize(1)}
+					>
+						FR
+					</Button>
+					<Button
+						variant="contained"
+						color="primary"
+						disabled={!SR}
+						onClick={(e) => claimPrize(2)}
+					>
+						SR
+					</Button>
+					<Button
+						variant="contained"
+						color="primary"
+						disabled={!TR}
+						onClick={(e) => claimPrize(3)}
+					>
+						TR
+					</Button>
+					<Button
+						variant="contained"
+						color="primary"
+						disabled={!fullHouse}
+						onClick={(e) => claimPrize(4)}
+					>
+						FH
+					</Button>
+				</ButtonGroup>
+			</div>
 		</>
 	);
 };
