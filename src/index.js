1f2aab41cb00ff93ab091f1261f7fb62e1a08f22
Sagar Mainkar <sagar.mainkar@sas.com>
2020-10-30 11:31:02 +0530

    Added Global theme provider

--- src/index.js
+++ src/index.js
@@ -7,13 +7,21 @@ import store from "./redux/store";
 import App from "./components/App";
 import Firebase, { FirebaseContext } from "./components/Firebase";
 
+import { ThemeProvider } from "@material-ui/core/styles";
+import createMuiTheme from "@material-ui/core/styles/createMuiTheme";
+import themeFile from "./utils/theme";
+
+const theme = createMuiTheme(themeFile);
+
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <FirebaseContext.Provider value={new Firebase()}>
-        <App />
-      </FirebaseContext.Provider>
-    </Provider>
+    <ThemeProvider theme={theme}>
+      <Provider store={store}>
+        <FirebaseContext.Provider value={new Firebase()}>
+          <App />
+        </FirebaseContext.Provider>
+      </Provider>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
